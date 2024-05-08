d244b8bfd69da5e181fb0f8524ce115764d80cf9
judge_happily <you3192001@gmail.com>
2024-05-08 16:08:03 +0900

    Initial Commit

---
+++ SwiftUIBasic/SwiftUIBasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicApp.swift
+//  SwiftUIBasic
+//
+//  Created by Jae hyung Kim on 5/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
