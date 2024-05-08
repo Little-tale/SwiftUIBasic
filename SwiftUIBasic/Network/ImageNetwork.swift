cd308d4b0e0da3555fef89aaa299b76b3799e247
judge_happily <you3192001@gmail.com>
2024-05-08 19:40:25 +0900

    Fetch: 테스트 전 수평 스택 뷰

--- SwiftUIBasic/Network/ImageNetwork.swift
+++ SwiftUIBasic/Network/ImageNetwork.swift
@@ -13,6 +13,15 @@ enum NetworkError: Error{
     
     case invalidImage
     
+    var errorMessage: String {
+        switch self {
+        case .invalidResponse:
+            return "요청 에렁"
+        case .invalidImage:
+            return "이미지 에렁"
+        }
+    }
+    
 }
 
 // Async 이미지도 있지만 이번엔 배운거를 접목 시켜보자
@@ -20,6 +29,7 @@ struct ImageNetwork {
     
     static let url = URL(string: "https://picsum.photos/200/300")!
     
+    static
     func fetchThumbnailAsyncAwait() async throws -> Image {
         
         let request = URLRequest(url: ImageNetwork.url, cachePolicy: .reloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 5)
--- SwiftUIBasic/Views/HorizenJHMovieView.swift
+++ SwiftUIBasic/Views/HorizenJHMovieView.swift
@@ -8,12 +8,43 @@
 import SwiftUI
 
 struct HorizenJHMovieView: View {
+    
+    @State var images: [Image?] = Array(repeating: nil, count: 10)
+    @State var ifError: NetworkError? = nil
+    @State var ifShowAlert: Bool = false
+    
     var body: some View {
         LazyHStack(alignment: .center, content: {
-            ForEach(1...10, id: \.self) { count in
-                /*@START_MENU_TOKEN@*/Text("Placeholder \(count)")/*@END_MENU_TOKEN@*/
+            ForEach(images.indices, id: \.self) { index in
+                if let image = images[index] {
+                    image
+                } else {
+                    Text("히히히히히히히")
+                }
             }
         })
+        .alert("네트워크 에러남", isPresented: $ifShowAlert, actions: {
+            Button("넵") {
+                
+            }
+        }, message: {
+            Text(ifError?.errorMessage ?? "엥?")
+        })
+        .onAppear {
+            Task {
+                for index in images.indices {
+                    do {
+                        async let image = try ImageNetwork.fetchThumbnailAsyncAwait()
+                    } catch {
+                        guard let error = error as? NetworkError else { 
+                            ifError = .invalidImage
+                            return
+                        }
+                        ifError = error
+                    }
+                }
+            }
+        }
     }
 }
 
